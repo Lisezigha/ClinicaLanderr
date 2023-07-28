3043a999e32c776460cb22c9b5e4e2d52cf4dcf6
Lisezigha <elisefadoni@gmail.com>
2023-07-28 11:33:22 -0300

    Coloquei os negocio

---
+++ ClinicaLanderr/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClinicaLanderr.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
