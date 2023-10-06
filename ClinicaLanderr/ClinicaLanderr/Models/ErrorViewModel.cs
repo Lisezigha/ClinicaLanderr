b694194a18e3f5bea0d5ebf79f22a00772eb4554
Lisezigha <elisefadoni@gmail.com>
2023-10-06 12:15:21 -0300

    fizemo nada

--- ClinicaLanderr/ClinicaLanderr/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ClinicaLanderr.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
